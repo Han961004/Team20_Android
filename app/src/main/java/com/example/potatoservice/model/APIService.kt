dc4e8b166f0b630a79fc87167034b5004260f248
Dong-Han <donghk77@naver.com>
2024-10-25 23:44:46 +0900

    feat : add sendKakaoAccessToken API

--- app/src/main/java/com/example/potatoservice/model/APIService.kt
+++ app/src/main/java/com/example/potatoservice/model/APIService.kt
@@ -1,6 +1,8 @@
 package com.example.potatoservice.model
 
+import com.example.potatoservice.model.remote.AccessTokenRequest
 import com.example.potatoservice.model.remote.ActivityResponse
+import com.example.potatoservice.model.remote.JwtResponse
 import com.example.potatoservice.model.remote.MarkerData
 import com.example.potatoservice.model.remote.UserInfo
 import com.example.potatoservice.model.remote.UserInterest
@@ -8,6 +10,7 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -24,8 +27,20 @@ interface APIService {
     ): Call<ResponseBody>
 
     @GET("/api/markers")
-    fun getMarkers(): Call<List<MarkerData>>
+    fun getMarkers(
 
+    ): Call<List<MarkerData>>
+
+    @POST("auth/kakao")
+    @Headers("Content-Type: application/json")
+    fun sendKakaoAccessToken(
+        @Body request: AccessTokenRequest
+    ): Call<JwtResponse>
+
+    @GET("/api/isUserRegistered")
+    fun isUserRegistered(
+        @Query("accessToken") kakaoAccessToken: String
+    ): Call<Boolean>
 
 
 
@@ -39,4 +54,10 @@ interface APIService {
         @Query("teenPossibleOnly") teenPossibleOnly: Boolean? = null,
         @Query("category") category: String? = null
     ): Call<ActivityResponse>
+
+
+
+
+
+
 }
