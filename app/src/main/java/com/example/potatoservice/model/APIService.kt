75302eb778cd162b8d56c33701a9210bae3629a7
Dong-Han <donghk77@naver.com>
2024-10-30 03:09:12 +0900

    feat : update /signup & /login

--- app/src/main/java/com/example/potatoservice/model/APIService.kt
+++ app/src/main/java/com/example/potatoservice/model/APIService.kt
@@ -1,52 +1,48 @@
 package com.example.potatoservice.model
 
-import com.example.potatoservice.model.remote.AccessTokenRequest
+import com.example.potatoservice.model.remote.AccessToken
 import com.example.potatoservice.model.remote.ActivityDetail
 import com.example.potatoservice.model.remote.ActivityResponse
-import com.example.potatoservice.model.remote.JwtResponse
+import com.example.potatoservice.model.remote.LoginRequest
 import com.example.potatoservice.model.remote.MarkerData
-import com.example.potatoservice.model.remote.UserInfo
-import com.example.potatoservice.model.remote.UserInterest
-import okhttp3.ResponseBody
+import com.example.potatoservice.model.remote.SendSignUpUserInfo
+import com.example.potatoservice.model.remote.SignUpRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIService {
 
+    /* 로그인
+    * AccessToken 넣고, 요청을 보내면 userInfo 및 jwt 반환
+     */
+    @POST("/api/login")
+    fun kakaoLogin(
+        @Body accessToken: AccessToken
+    ): Call<LoginRequest>
+
+    /* 회원 가입
+    * jwtToken 넣고, sendSignUpUser 객체에 담아서 보내고, 가입 여부 메시지 반환
+     */
     @POST("/api/signup")
     fun sendUserInfo(
-        @Body userInfo: UserInfo
-    ): Call<Void>
+        @Header("Authorization") jwtToken: String,
+        @Body signUpInfo: SendSignUpUserInfo
+    ): Call<SignUpRequest>
 
-    @POST("/api/interests")
-    fun sendUserInterest(
-        @Body userInterest: UserInterest
-    ): Call<ResponseBody>
 
+
+    // 지도 맵 마커
     @GET("/api/markers")
     fun getMarkers(
-
     ): Call<List<MarkerData>>
 
-    @POST("auth/kakao")
-    @Headers("Content-Type: application/json")
-    fun sendKakaoAccessToken(
-        @Body request: AccessTokenRequest
-    ): Call<JwtResponse>
-
-    @GET("/api/isUserRegistered")
-    fun isUserRegistered(
-        @Query("accessToken") kakaoAccessToken: String
-    ): Call<Boolean>
 
 
-
-    // API method to get activities
     @GET("/api/v1/activities")
     fun getActivities(
         @Query("page") page: Int,
@@ -61,6 +57,4 @@ interface APIService {
     fun getActivityDetail(
         @Path("activity_id") activityId: Int
     ): Call<ActivityDetail>
-
-    
 }
