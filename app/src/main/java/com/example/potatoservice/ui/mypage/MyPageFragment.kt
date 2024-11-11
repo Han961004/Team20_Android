683a86c4900ba75bbcb4f537939d248b1d24da4f
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2024-11-11 18:25:34 +0900

    fix: Log import 없음

--- app/src/main/java/com/example/potatoservice/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/potatoservice/ui/mypage/MyPageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.potatoservice.ui.mypage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
